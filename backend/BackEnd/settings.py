bcdca1fa0bb594db7d5eab52f1ee00b5676f791a
Naveed <naveedilyas321@gmail.com>
2023-09-05 22:06:16 +0500

    changes

--- backend/BackEnd/settings.py
+++ backend/BackEnd/settings.py
@@ -170,6 +170,8 @@ SIMPLE_JWT = {
 }
 
 CORS_ALLOWED_ORIGINS = [
-    "http://localhost:3000",
-    "http://127.0.0.1:3000",
+    "http://localhost:5173",
+    "http://127.0.0.1:8000",
 ]
+
+CORS_ALLOW_ALL_ORIGINS = True

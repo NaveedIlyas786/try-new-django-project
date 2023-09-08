d56b4ab7085d501166b1b902c1f5c25626380fbe
Mubeen <mubeenjutt9757@gmail.com>
2023-09-08 16:30:30 +0500

    add some database

--- backend/projects/admin.py
+++ backend/projects/admin.py
@@ -2,6 +2,19 @@ from django.contrib import admin
 from .models import Project_detail,Project,Company
 # Register your models here.
 
-admin.site.register(Project) 
-admin.site.register(Project_detail)
-admin.site.register(Company)
+class CompanyAdmin(admin.ModelAdmin):
+    list_display = ('id', 'Cmpny_Name')
+
+
+class ProjectAdmin(admin.ModelAdmin):
+    list_display = ('id', 'prjct_Name', 'company')
+
+
+class ProjectDetailAdmin(admin.ModelAdmin):
+    list_display = ('id', 'drctry_Name', 'prjct_ID', 'prnt_ID', 'file_Name', 'output_Table_Name')
+
+
+# Register the models with their respective admin views
+admin.site.register(Company, CompanyAdmin)
+admin.site.register(Project, ProjectAdmin)
+admin.site.register(Project_detail, ProjectDetailAdmin)
--- backend/projects/models.py
+++ backend/projects/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 class Company(models.Model):
-    Cmpny_Name = models.CharField(max_length=255)
+    Cmpny_Name = models.CharField(verbose_name="Company Name",max_length=255)
 
     def __str__(self):
         return self.Cmpny_Name
@@ -9,7 +9,7 @@ class Company(models.Model):
 
 class Project(models.Model):
     company=models.ForeignKey(Company, on_delete=models.CASCADE)
-    prjct_Name = models.CharField(max_length=255)
+    prjct_Name = models.CharField(verbose_name="Project Name",max_length=255)
 
     def __str__(self):
         return self.prjct_Name
@@ -18,8 +18,8 @@ class Project_detail(models.Model):
     drctry_Name = models.CharField(verbose_name="Folder Name",max_length=255)
     prjct_ID = models.ForeignKey(Project,verbose_name="Project", on_delete=models.CASCADE)
     prnt_ID = models.PositiveIntegerField(verbose_name="Folder Parent ID",null=True, blank=True)  # Set null=True here
-    file_Name = models.CharField(verbose_name="File Name",max_length=100, null=True, blank=True)
-    output_Table_Name = models.CharField(verbose_name="Table Name",max_length=100, null=True, blank=True)
+    file_Name = models.CharField(verbose_name="File Name",max_length=100, null=True, blank=True)  # Set null=True here
+    output_Table_Name = models.CharField(verbose_name="Table Name",max_length=100, null=True, blank=True)  # Set null=True here
 
 
 

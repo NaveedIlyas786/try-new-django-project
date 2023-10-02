aac1cf9141c01d36338e71b575c6e25bf99252d2
Naveed <naveedilyas321@gmail.com>
2023-10-02 17:35:28 +0500

    dashbord wprk satrt

--- frontend/src/pages/dashboard/Dashboard.jsx
+++ frontend/src/pages/dashboard/Dashboard.jsx
@@ -3,9 +3,11 @@ import "./Dashboard.css"
 const Dashboard = () => {
   return (
     <div className='dashboard'>
-    {/* <div className='bgimg'></div> */}
-      <h1>Dashboard</h1>
-     
+     <div className=' bg-primary p-4 ProjectStatus'>1</div>
+     <div className=' bg-primary p-4 ProjectStatus'>2</div>
+     <div className=' bg-primary p-4 ProjectStatus'>3</div>
+     <div className=' bg-primary p-4 ProjectStatus'>4</div>
+     <div className=' bg-primary p-4 ProjectStatus'>5</div>
     </div>
   )
 }

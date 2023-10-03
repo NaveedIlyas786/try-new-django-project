6e681f62668bea27ac96c4c771b1db83a5665116
Naveed <naveedilyas321@gmail.com>
2023-10-03 23:53:07 +0500

    dashboard also push

--- frontend/src/pages/dashboard/Dashboard.jsx
+++ frontend/src/pages/dashboard/Dashboard.jsx
@@ -1,13 +1,25 @@
 import React from "react";
 import "./Dashboard.css";
-import React from "react";
-import "./Dashboard.css";
 
 const Dashboard = () => {
   return (
     <>
       <div className=" container dashboard ">
         <div className=" row projectStatus justify-content-around">
+          <div className=" col-md-2 p-2 ProjectStatus  text-center">
+            <h4 className="pt-2">7</h4>
+            <p>
+              <i className="fa-solid fs-5 fa fa-square-check check"></i>
+            </p>
+            <h5>Won</h5>
+          </div>
+          <div className=" col-md-2 p-2 ProjectStatus  text-center">
+            <h4 className="pt-2" >7</h4>
+            <p>
+              <i className="fa-solid  fa-question fs-5 pend"></i>
+            </p>
+            <h5>pending</h5>
+          </div>
           <div className=" col-md-2 p-2 text-center ProjectStatus">
             <h4 className="pt-2">10</h4>
             <p>
@@ -18,26 +30,12 @@ const Dashboard = () => {
               <i className="fa-solid fa-square-this-way-up "></i>
             </p>
           </div>
-          <div className=" col-md-2 p-2 ProjectStatus  text-center">
-            <h4 className="pt-2" >7</h4>
-            <p>
-              <i className="fa-solid  fa-question fs-5 pend"></i>
-            </p>
-            <h5>pending</h5>
-          </div>
-          <div className=" col-md-2 p-2 ProjectStatus  text-center">
-            <h4 className="pt-2">7</h4>
-            <p>
-              <i className="fa-solid fs-5 fa fa-square-check check"></i>
-            </p>
-            <h5>Won</h5>
-          </div>
           <div className=" col-md-2 text-center p-2 ProjectStatus">
             <h4 className="pt-2">10</h4>
             <p>
               <i className="fa-solid fa fs-5 fa-square-xmark mark"></i>
             </p>
-            <h5>Lost</h5>
+            <h5>Rejected</h5>
           </div>
         </div>
       </div>
@@ -267,129 +265,6 @@ const Dashboard = () => {
                 alt="My Graph"
               />
             </div>
-          </div>
-        </div>
-      </div>
-    </>
-  );
-};
-    <>
-      <div className=" container dashboard ">
-        <div className=" row projectStatus justify-content-around">
-          <div className=" col-md-2 p-2 ProjectStatus  text-center">
-            <p>
-              <i class="fa-solid fs-5 fa fa-square-check"></i>
-            </p>
-            <h4>7</h4>
-            <h5>Won</h5>
-          </div>
-          <div className=" col-md-2 p-2 ProjectStatus  text-center">
-            <p>
-            <i class="fa-solid  fa-question fs-5"></i>
-            </p>
-            <h4>7</h4>
-            <h5>pending</h5>
-          </div>
-          <div className=" col-md-2 p- text-center ProjectStatus">
-          <p><i class="fa-solid fa-spinner fs-5 "></i></p>
-          <h4>10</h4>
-            <h5>Working</h5>
-            <p>
-              <i class="fa-solid fa-square-this-way-up"></i>
-            </p>
-          </div>
-          <div className=" col-md-2 p-4 ProjectStatus">
-            <h5>Rejected</h5>
-            <p>
-              <i class="fa-solid fa fs-5 fa-square-xmark"></i>
-            </p>
-          </div>
-          <div className=" col-md-2 p-4 ProjectStatus">
-          
-        </div>
-        </div>
-
-        <div className="middlePortion ">
-          <div className="table-responsive bk ">
-            <table className="table table-striped  table-bordered table-hover">
-              <thead className="proposalHeader">
-                <tr>
-                  <th>Start Date</th>
-                  <th>Time</th>
-                  <th>Project Name</th>
-                  <th>Area</th>
-                  <th>Estimator</th>
-                  <th>Status</th>
-                  <th>Bidders</th>
-                  <th>Actions</th>
-                </tr>
-              </thead>
-              <tbody className="cursor-pointer jktable bg-info jloop">
-                <tr>
-                  <td className="mytd centered-td">1</td>
-                  <td className="mytd centered-td">1</td>
-                  <td className="mytd myproject centered-td">3</td>
-                  <td className="mytd centered-td">5</td>
-                  <td className="mytd centered-td">5</td>
-                  <td className="mytd centered-td">5</td>
-                  <td className="mytd centered-td">5</td>
-                  <td className="mytd centered-td">5</td>
-                </tr>
-                <tr>
-                  <td className="mytd centered-td">1</td>
-                  <td className="mytd centered-td">1</td>
-                  <td className="mytd myproject centered-td">3</td>
-                  <td className="mytd centered-td">5</td>
-                  <td className="mytd centered-td">5</td>
-                  <td className="mytd centered-td">5</td>
-                  <td className="mytd centered-td">5</td>
-                  <td className="mytd centered-td">5</td>
-                </tr>
-                <tr>
-                  <td className="mytd centered-td">1</td>
-                  <td className="mytd centered-td">1</td>
-                  <td className="mytd myproject centered-td">3</td>
-                  <td className="mytd centered-td">5</td>
-                  <td className="mytd centered-td">5</td>
-                  <td className="mytd centered-td">5</td>
-                  <td className="mytd centered-td">5</td>
-                  <td className="mytd centered-td">5</td>
-                </tr>
-                <tr>
-                  <td className="mytd centered-td">1</td>
-                  <td className="mytd centered-td">1</td>
-                  <td className="mytd myproject centered-td">3</td>
-                  <td className="mytd centered-td">5</td>
-                  <td className="mytd centered-td">5</td>
-                  <td className="mytd centered-td">5</td>
-                  <td className="mytd centered-td">5</td>
-                  <td className="mytd centered-td">5</td>
-                </tr>
-                <tr>
-                  <td className="mytd centered-td">1</td>
-                  <td className="mytd centered-td">1</td>
-                  <td className="mytd myproject centered-td">3</td>
-                  <td className="mytd centered-td">5</td>
-                  <td className="mytd centered-td">5</td>
-                  <td className="mytd centered-td">5</td>
-                  <td className="mytd centered-td">5</td>
-                  <td className="mytd centered-td">5</td>
-                </tr>
-                <tr>
-                  <td className="mytd centered-td">1</td>
-                  <td className="mytd centered-td">1</td>
-                  <td className="mytd myproject centered-td">3</td>
-                  <td className="mytd centered-td">5</td>
-                  <td className="mytd centered-td">5</td>
-                  <td className="mytd centered-td">5</td>
-                  <td className="mytd centered-td">5</td>
-                  <td className="mytd centered-td">5</td>
-                </tr>
-              </tbody>
-            </table>
-          </div>
-          <div className="graphPortion bk">
-            <p>Here Graph will be Displayed !</p>
           </div>
         </div>
       </div>

7e1db1d3ca076d8bc7c94ccfcf3671f9bdda7098
Naveed <naveedilyas321@gmail.com>
2023-10-15 17:44:52 +0500

    changes

--- frontend/src/pages/signUp/AdminDecisionpage.jsx
+++ frontend/src/pages/signUp/AdminDecisionpage.jsx
@@ -5,27 +5,14 @@ import "./decision.css";
 const AdminDecisionpage = () => {
   const navigate = useNavigate();
   return (
-    // <div className="admindecisionPage">
-    //   <h1>
-    //     Please Take decision and chose ( Approval/Rejection ) regarding
-    //     registration Request
-    //   </h1>
-
-    //   <div className="d-flex gap-3">
-    //     <button onClick={()=>{
-    //         navigate("/")
-    //     }} className="btn btn-success">Approve</button>
-    //     <button onClick={()=>{
-    //         navigate("/rejectionPage")
-    //     }} className="btn btn-danger">Reject</button>
-    //   </div>
-    // </div>
+    
     <div className="admindecisionPage">
       <div className="contentwaiting px-5">
         <h1>
           Please Take decision and chose ( Approval/Rejection ) regarding
           registration Request
         </h1>
+        <h2>"User_Name" is sending you the request for joining the DMS System and needs your approval</h2>
         <div className="d-flex justify-content-center gap-3">
           <button
             onClick={() => {

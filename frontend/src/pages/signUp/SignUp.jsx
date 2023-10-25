33a8577c8290ecc27a8b3c0dfd1bca0b44a07210
Malik Waqar <mw7028449@gmail.com>
2023-10-25 18:57:36 +0500

    sign up final

--- frontend/src/pages/signUp/SignUp.jsx
+++ frontend/src/pages/signUp/SignUp.jsx
@@ -31,50 +31,23 @@ const Signup = () => {
     setSuccessMessage("");
     setSubmitbtnClicked(true);
 
-    // Check if the email already exists before proceeding
-
     // Check for empty fields
     if (!full_Name || !email || !password || !password2) {
-      // setError("All fields are required");
-      setError(
-        <>
-          {/* <div className="modal-overlay">
-            <div className="popuppass">
-              {/* <p>All Fields are required</p> */}
-              {alert("All Fields are required")}
-            </div>
-          </div> */}
-          {alert("All Fields are required")}
-        </>
-      );
-
-      // Use setTimeout to remove the error message after 1 second
+      setError(<>{alert("All Fields are required")}</>);
       setTimeout(() => {
         setError(null); // Remove the error message
-      },);
+      });
       return;
     }
-    //if email is invalid
 
+    //if email is invalid
     const emailPattern = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i;
     const isValidEmail = emailPattern.test(email);
     if (!isValidEmail) {
-      setError(
-        <>
-          {/* <div className="modal-overlay">
-            <div className="popuppass">
-              {/* <p>Invalid Email</p> */}
-              {alert("Your Email is Invalid")}
-            </div>
-          </div> */}
-          {alert("Invalid Email")}
-        </>
-      );
-
-      // Use setTimeout to remove the error message after 1 second
+      setError(<>{alert("Invalid Email")}</>);
       setTimeout(() => {
         setError(null); // Remove the error message
-      },); // 1000 milliseconds = 1 second
+      }); // 1000 milliseconds = 1 second
 
       useEffect(() => {
         // Effect to handle the response from the server
@@ -84,10 +57,6 @@ const Signup = () => {
       }, [email]);
 
       const checkDuplicateEmail = (email) => {
-        // Replace this with your own logic to check for duplicate emails
-        // Example: If your backend returns a response like { isDuplicate: true }, or { isDuplicate: false }
-        // You can adjust this code accordingly
-
         axios
           .post("http://127.0.0.1:8000/api/user/register/", { email })
 
@@ -106,24 +75,10 @@ const Signup = () => {
 
     // Check if password and confirm password match
     if (password !== password2) {
-      // setError("Passwords do not match");
-      setError(
-        <>
-          {/* <div className="modal-overlay">
-            <div className="popuppass">
-              {/* <p> Password do not match</p> */}
-              {alert("Password do not match")}
-            </div>
-          </div> */}
-          {alert("Password do not match")}
-
-        </>
-      );
-
-      // Use setTimeout to remove the error message after 1 second
+      setError(<>{alert("Password do not match")}</>);
       setTimeout(() => {
         setError(null); // Remove the error message
-      }, );
+      });
       return;
     }
 
@@ -132,47 +87,22 @@ const Signup = () => {
       /^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[@$!%*?&])[A-Z][A-Za-z\d@$!%*?&]{7,}$/;
     const isValidPassword = passwordRegex.test(password);
     if (password.length < 8) {
-      // setError("Password length should be equal or greater than 8");
       setError(
-        <>
-          {/* <div className="modal-overlay">
-            <div className="popuppass">
-              <p>Password length should be Equal or greater than8</p>
-            </div>
-          </div> */}
-          {alert("Password length should be Equal or greater than 8")}
-
-        </>
+        <>{alert("Password length should be Equal or greater than 8")}</>
       );
-
-      // Use setTimeout to remove the error message after 1 second
       setTimeout(() => {
         setError(null); // Remove the error message
-      }, );
+      });
       return;
     }
     if (!isValidPassword) {
-      setError(
-        <>
-          {/* <div className="modal-overlay">
-            <div className="popuppass">
-              {/* <p>Invalid Password</p> */}
-              {alert("Password is not Matched")}
-            </div>
-          </div> */}
-          {alert("Invalid Password")}
-
-        </>
-      );
-
-      // Use setTimeout to remove the error message after 1 second
+      setError(<>{alert("Invalid Password")}</>);
       setTimeout(() => {
         setError(null); // Remove the error message
-      }, ); // 1000 milliseconds = 1 second
-
+      }); // 1000 milliseconds = 1 second
       return;
     }
-   
+
     try {
       const userData = {
         full_Name: full_Name,
@@ -188,41 +118,31 @@ const Signup = () => {
           // Handle success response from the backend (if needed)
           console.log("User registration successful:", response);
           setSuccessMessage(
-            <div className="modal-overlay">
-              <div className="popupsuccess">
-                <p>Registration Successfull</p>
-                {alert("Registration Successfull")}
-              </div>
-            </div>
-
+            alert("Your Request has been sent to the admin for Approval")
           );
-          // setTimeout(() => {
-          //   navigate("/waitingPage");
-          //   // navigate("/");
-          // }, 1000);
-          // Redirect the user to a success page or do something else
+
+          // Clear the form fields
+          setfull_Name("");
+          setEmail("");
+          setPassword("");
+          setpassword2("");
+
+          setTimeout(() => {
+            // navigate("/waitingPage");
+            // navigate("/");
+          }, 1000);
         })
         .catch((error) => {
           // Handle error response from the backend
           console.error("User registration failed:", error);
-        
+
           // Adjust this to fit the actual error structure you’re receiving
           if (
             error &&
             error.email &&
             error.email.includes("user with this Email already exists.")
           ) {
-            setError(
-              <>
-                {/* <div className="modal-overlay">
-                  <div className="popuppass">
-                    <p>User with this Email already exists.</p>
-                  </div>
-                </div> */}
-                {alert("User this Email is already exists")}
-              </>
-            );
-            
+            setError(<>{alert("User this Email is already exists")}</>);
           } else {
             setError("Registration failed. Please try again.");
           }
@@ -244,21 +164,9 @@ const Signup = () => {
         {SubmitbtnClicked && (
           <>
             {error ? (
-              <div
-              // className="error bg-danger w-100 p-2 text-center rounded"
-              // style={{ color: "white" }}
-              >
-                {error}
-              </div>
+              <div>{error}</div>
             ) : (
-              successMessage && (
-                <div
-                // className="success bg-success w-100 p-2 text-center rounded"
-                // style={{ color: "white" }}
-                >
-                  {successMessage}
-                </div>
-              )
+              successMessage && <div>{successMessage}</div>
             )}
           </>
         )}
@@ -294,8 +202,9 @@ const Signup = () => {
           </span>
         </div>
         <div className="password-validation ">
-          <p className="format">First capital letter,At least one number and 
-          special character</p>
+          <p className="format">
+            First capital letter,At least one number and special character
+          </p>
         </div>
 
         <div className="passfield pass2">
@@ -317,12 +226,6 @@ const Signup = () => {
         <button className="submitbtn" onClick={handleRegister}>
           Send Request
         </button>
-        {/* <p className="signuplink">
-          Already have an account:{" "}
-          <Link className="Link" to="/">
-            Login
-          </Link>
-        </p> */}
       </div>
     </div>
   );

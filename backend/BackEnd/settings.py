a6c6185ab9f1c9c1be31e5adea7382eeea4d795f
Naveed <naveedilyas321@gmail.com>
2024-04-04 21:59:24 +0500

    backend deployment try 1st on digital Ocean

--- backend/BackEnd/settings.py
+++ backend/BackEnd/settings.py
@@ -198,7 +198,3 @@ CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
 ]
-
-import os
-STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')

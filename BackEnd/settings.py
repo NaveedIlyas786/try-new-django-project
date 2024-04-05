6606204d6e8960a8bbde0f6bbca3f9b8e8a38718
Naveed <naveedilyas321@gmail.com>
2024-04-06 03:00:32 +0500

    Add allowed hosts

--- BackEnd/settings.py
+++ BackEnd/settings.py
@@ -219,6 +219,7 @@ CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
     'https://dms-cms-project.vercel.app',
+    "https://try-new-django-project.vercel.app",
     'http://127.0.0.1:5173',
 ]
 
